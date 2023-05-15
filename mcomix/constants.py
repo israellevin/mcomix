ad0a0624cac408b0c59eb3296e1afbe5925299f3
Oddegamra <oddegamra@users.sourceforge.net>
2023-05-15 19:17:32 +0200

    doc: Change version to 2.1.1 in preparation of new version

--- mcomix/constants.py
+++ mcomix/constants.py
@@ -6,7 +6,7 @@ import os
 from mcomix import tools
 
 APPNAME = 'MComix'
-VERSION = '2.1.1-dev0'
+VERSION = '2.1.1'
 
 HOME_DIR = tools.get_home_directory()
 CONFIG_DIR = tools.get_config_directory()
