a88e8860202da10160f60d13b53dc570ed36868b
Oddegamra <oddegamra@users.sourceforge.net>
2023-06-13 19:06:25 +0200

    doc: Annotate tools test

--- test/test_tools.py
+++ test/test_tools.py
@@ -2,13 +2,14 @@ import unittest
 
 from mcomix import tools
 
+
 class TestAlphanumericSort(unittest.TestCase):
-    def test_numbers_are_ordered_naturally(self):
+    def test_numbers_are_ordered_naturally(self) -> None:
         lst = ['10.jpg', '2.jpg']
         tools.alphanumeric_sort(lst)
         self.assertListEqual(lst, ['2.jpg', '10.jpg'])
 
-    def test_sort_with_mixed_number_and_string_files(self):
+    def test_sort_with_mixed_number_and_string_files(self) -> None:
         lst = ['text_2.jpg', '2_text.jpg']
         tools.alphanumeric_sort(lst)
         self.assertListEqual(lst, ['2_text.jpg', 'text_2.jpg'])
