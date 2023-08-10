ee1f9aa9019a16860df5bde3d9f40a8efa76f419
FeRD (Frank Dana) <ferdnyc@gmail.com>
2023-08-10 14:41:12 -0400

    Enhance, type-annotate mcomix.transform
    - To make mcomix.transform.Transform type-checkable without changing
      functionality, create a new class mcomix.transform.Matrix which
      holds transform data. mcomix.transform.Transform.ID and the other
      "static" transforms are now Matrix members of the Transform class.
      mcomix.transform.Transform.__call__() returns Matrix instances,
      as do the "converting" methods like Transform.from_flips(), etc.
    - Matrix is enhanced with various dunder methods:
      - __bool__: return true if not the identity matrix
      - __eq__/__ne__: Compare two Matrix instances
      - __add__/__radd__: Combine Matrix instances
    - The latter two dunder methods allow replacing code like
        xform = xform.and_then(Transform.from_flips(True, False))
      with
        xform += Transform.from_flips(True, False)
    - Matrix also has three converting+combining instance methods:
      rotated(), scaled(), and flipped().
      These return a new Matrix instance created by combining the
      original instance with the result of Transform.from_rotations(),
      Transform.from_scales(), or Transform.from_flips(), respectively.
    - Matrix.scaled() accepts one or two arguments. The one-argument form
      will scale both axes by the same factor.

--- mcomix/transform.py
+++ mcomix/transform.py
@@ -1,76 +1,129 @@
 """ A series of a few simple linear transforms. """
 
-from functools import reduce
+# Allow class name in annotations while still defining class
+from __future__ import annotations
 
-class Transform(object): # 2D only
+import sys
+from typing import Optional, Tuple, Type, TYPE_CHECKING
 
-    @staticmethod
-    def from_rotation(deg):
-        if deg == 0:
-            return Transform.ID
-        if deg == 90:
-            return Transform.ROT90
-        if deg == 180:
-            return Transform.ROT180
-        if deg == 270:
-            return Transform.ROT270
-        raise ValueError("illegal rotation angle: " + str(deg))
+if TYPE_CHECKING and sys.version_info[:2] >= (3, 11):
+    from typing import TypeAlias
+    MatrixData: TypeAlias = Tuple[float, float, float, float]
+    Scales: TypeAlias = Tuple[float, float]
+    Rotation: TypeAlias = int
+    Flips: TypeAlias = Tuple[bool, bool]
+    ImageTransforms: TypeAlias = Tuple[Scales, Rotation, Flips]
+elif TYPE_CHECKING:
+    MatrixData = "Tuple[float, float, float, float]"
+    Scales = "Tuple[float, float]"
+    Rotation = "int"
+    Flips = "Tuple[bool, bool]"
+    ImageTransforms = "Tuple[Scales, Rotation, Flips]"
 
-    @staticmethod
-    def from_scales(s0, s1):
-        if s0 == 0 or s1 == 0:
-            raise ValueError("illegal scaling factor(s): " + str((s0, s1)))
-        return Transform(s0, 0, 0, s1)
+class Matrix:
+    """Simple linear transformations represented as a 2x2 matrix.
 
-    @staticmethod
-    def from_flips(f0, f1):
-        if (f0, f1) == (False, False):
-            return Transform.ID
-        if (f0, f1) == (True, False):
-            return Transform.INVX
-        if (f0, f1) == (False, True):
-            return Transform.INVY
-        if (f0, f1) == (True, True):
-            return Transform.ROT180
-        raise ValueError("illegal flips: " + str((f0, f1)))
+    Note that the current implementation does not support arbitrary
+    matrices. Instead, the predefined Matrix objects and conversion
+    functions in the Transform class should be used to create Matrix
+    objects."""
 
-    @staticmethod
-    def from_image_transforms(t):
-        return Transform.from_scales(*t[0]).and_then_all(
-            Transform.from_rotation(t[1]), Transform.from_flips(*t[2]))
+    def __init__(self, m1: float, m2: float, m3: float, m4: float) -> None:
+        """Initialize a row-major transformation matrix."""
+        self.m: MatrixData = (m1, m2, m3, m4)
 
-    def __init__(self, *matrix):
-        """ Creates a new transform described by a row-major matrix. Note that
-        the current implementation does not support arbitrary matrices. Instead,
-        one should use one of the eight predefined matrices or one of the
-        constructor-like functions. """
-        self.m = matrix
-
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.m)
 
-    def and_then(self, next_transform):
-        """ Returns a new matrix describing the current transform followed by
-        next_transform. """
-        dp = lambda i11, i21, i12, i22: next_transform.m[i11] * self.m[i21] + \
-            next_transform.m[i12] * self.m[i22]
-        return Transform(
-            dp(0, 0, 1, 2),
-            dp(0, 1, 1, 3),
-            dp(2, 0, 3, 2),
-            dp(2, 1, 3, 3)
+    def __repr__(self) -> str:
+        args = ", ".join([str(x) for x in self.m])
+        return f'Matrix({args})'
+
+    def __reduce__(self) -> Tuple[Type[Matrix], MatrixData]:
+        return (self.__class__, self.m)
+
+    def __bool__(self) -> bool:
+        """Does this matrix perform any operations (is not identity)?"""
+        return self != Transform.ID
+
+    def __add__(self, other: object) -> Matrix:
+        """Combine Matrices: self + other."""
+        if not isinstance(other, Matrix):
+            return NotImplemented
+        a = self.m
+        b = other.m
+        return Matrix(
+            b[0] * a[0] + b[1] * a[2],
+            b[0] * a[1] + b[1] * a[3],
+            b[2] * a[0] + b[3] * a[2],
+            b[2] * a[1] + b[3] * a[3],
         )
 
-    def and_then_all(self, *next_transforms):
-        """ Returns a new matrix describing the current transform followed by
-        each of the transforms in next_transforms in the order given. """
-        return reduce(Transform.and_then, next_transforms, self)
+    def __radd__(self, other: object) -> Matrix:
+        """Combine Matrices: other + self."""
+        if isinstance(other, Matrix):
+            return other + self
+        return NotImplemented
 
-    def swaps_axes(self):
-        """ Determines whether this transform includes a transpose of the axes. """
+    def __eq__(self, other: object) -> bool:
+        """Test equality: self == other."""
+        if isinstance(other, Matrix):
+            return len(self.m) == len(other.m) and all([
+                self.m[i] == other.m[i]
+                for i in range(len(self.m))
+            ])
+        return NotImplemented
+
+    def __ne__(self, other: object) -> bool:
+        """Test inequality: self != other."""
+        if isinstance(other, Matrix):
+            return not self == other
+        return NotImplemented
+
+    def and_then(self, next: Matrix) -> Matrix:
+        """The matrix result of transforming self by 'next'."""
+        return self + next
+
+    def and_then_all(self, *nexts: Matrix) -> Matrix:
+        """The matrix resulting from self transformed by each 'nexts' in turn."""
+        return sum(nexts, start=self)
+
+    def swaps_axes(self) -> bool:
+        """ Determines whether this matrix includes a transpose of the axes. """
         return self.m[0] == 0
 
-    def to_image_transforms(self):
+    def rotated(self, deg: int) -> Matrix:
+        """The matrix result of rotating self by 'deg' degrees."""
+        return self + Transform.from_rotation(deg)
+
+    def scaled(self, s0: float, s1: Optional[float] = None) -> Matrix:
+        """The matrix result of scaling self by the provided factor(s).
+
+        t.scale(s) will scale both dimensions by s.
+        t.scale(s0, s1) will scale x by s0, and y by s1."""
+        if s1 is None:
+            s1 = s0
+        return self + Transform.from_scales(s0, s1)
+
+    def flipped(self, x: bool = False, y: bool = False) -> Matrix:
+        """The matrix result of applying the specified flips."""
+        if x and y:
+            return self + Transform.ROT180
+        if x:
+            return self + Transform.INVX
+        if y:
+            return self + Transform.INVY
+        return self
+
+    def flipped_x(self) -> Matrix:
+        """The matrix result of flipping self horizontally."""
+        return self.flipped(x=True)
+
+    def flipped_y(self) -> Matrix:
+        """The matrix result of flipping self vertically."""
+        return self.flipped(y=True)
+
+    def to_image_transforms(self) -> ImageTransforms:
         """ Decomposes the transform to a sequence of hints to basic transform
         instructions typically found in image processing libraries. The sequence
         will refer to the positive scaling factors to be applied for each axis
@@ -80,28 +133,79 @@ class Transform(object): # 2D only
         factors for the corresponding axes, r is one of (0, 90, 180, 270),
         referring to the clockwise rotation to be applied, and f is a sequence
         of bools where True refers to the corresponding axis to be flipped. """
-        s = tuple(map(lambda i, j: abs(self.m[i] + self.m[j]), (0, 1), (2, 3)))
-        r, f0 = (90, True) if self.swaps_axes() else (0, False)
-        f1 = self.m[2] < 0 or self.m[3] < 0
-        f0 ^= self.m[0] < 0 or self.m[1] < 0
-        if f0 and f1:
-            f0, f1 = False, False
+        s: Scales = (abs(self.m[0] + self.m[1]), abs(self.m[2] + self.m[3]))
+        r: Rotation = 90 if self.swaps_axes() else 0
+        f: Flips = (
+            self.swaps_axes() ^ (self.m[0] < 0 or self.m[1] < 0),
+            (self.m[2] < 0 or self.m[3] < 0)
+        )
+        if all(f):
+            f = (False, False)
             r += 180
-        if f0 and r == 90:
+        if f[0] and r == 90:
             # Try to avoid horizontal flips because they tend to be slow, given
             # a certain combination of hardware, memory layout and libraries.
-            f0, f1 = False, True
+            f = (False, True)
             r = 270
-        return (s, r, (f0, f1))
+        return (s, r, f)
 
-Transform.ID = Transform(1, 0, 0, 1)
-Transform.ROT90 = Transform(0, -1, 1, 0)
-Transform.ROT180 = Transform(-1, 0, 0, -1)
-Transform.ROT270 = Transform(0, 1, -1, 0)
-Transform.INVX = Transform(-1, 0, 0, 1)
-Transform.INVY = Transform(1, 0, 0, -1)
-Transform.TRP = Transform(0, 1, 1, 0)
-Transform.TRPINV = Transform(0, -1, -1, 0)
 
+class Transform(Matrix):
+
+    ID = Matrix(1, 0, 0, 1)
+    ROT90 = Matrix(0, -1, 1, 0)
+    ROT180 = Matrix(-1, 0, 0, -1)
+    ROT270 = Matrix(0, 1, -1, 0)
+    INVX = Matrix(-1, 0, 0, 1)
+    INVY = Matrix(1, 0, 0, -1)
+    TRP = Matrix(0, 1, 1, 0)
+    TRPINV = Matrix(0, -1, -1, 0)
+
+    @classmethod
+    def __call__(cls, *values: float) -> Matrix:
+        """Construct a new Matrix by calling Transform(values)."""
+        return Matrix(*values)
+
+    @classmethod
+    def from_rotation(cls, deg: int) -> Matrix:
+        """Get the predefined Matrix for a supported rotation."""
+        if abs(deg) not in (0, 90, 180, 270):
+            raise ValueError("illegal rotation angle: " + str(deg))
+        if deg < 0: deg = 360 + deg
+        if deg == 90:
+            return cls.ROT90
+        if deg == 180:
+            return cls.ROT180
+        if deg == 270:
+            return cls.ROT270
+        return cls.ID
+
+    @classmethod
+    def from_scales(cls, s0: float, s1: float) -> Matrix:
+        """Get a Matrix representing the given x and y scales."""
+        if s0 == 0 or s1 == 0:
+            raise ValueError("illegal scaling factor(s): " + str((s0, s1)))
+        return Matrix(s0, 0, 0, s1)
+
+    @classmethod
+    def from_flips(cls, x: bool, y: bool) -> Matrix:
+        """Get the predefined Matrix representing the given x and y flips."""
+        if x and y:
+            return cls.ROT180
+        if x:
+            return cls.INVX
+        if y:
+            return cls.INVY
+        return cls.ID
+
+    @classmethod
+    def from_image_transforms(cls, t: ImageTransforms) -> Matrix:
+        """Create a Matrix transform from a set of image transforms."""
+        s, r, f = t[0:3]
+        return (
+            cls.from_scales(*s)
+            + cls.from_rotation(r)
+            + cls.from_flips(*f)
+        )
 
 # vim: expandtab:sw=4:ts=4
