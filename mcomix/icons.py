7d42c0882b5a4dab2f8c4b2b168c52966d420577
Oddegamra <oddegamra@users.sourceforge.net>
2023-09-07 14:32:21 +0200

    fix: Add new 256x256 icon to runtime application icons

--- mcomix/icons.py
+++ mcomix/icons.py
@@ -11,7 +11,7 @@ def mcomix_icons():
     """ Returns a list of differently sized pixbufs for the
     application icon. """
 
-    sizes = ('16x16', '32x32', '48x48')
+    sizes = ('16x16', '32x32', '48x48', '256x256')
     pixbufs = [
         image_tools.load_pixbuf_data(
             pkgutil.get_data('mcomix', 'images/%s/mcomix.png' % size)
