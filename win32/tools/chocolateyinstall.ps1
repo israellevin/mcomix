29f0d072704d7a83ebd7b54ddf7fc488df893a6a
Oddegamra <oddegamra@users.sourceforge.net>
2023-09-14 20:07:22 +0200

    build: Use MSI installer in choco package

--- win32/tools/chocolateyinstall.ps1
+++ win32/tools/chocolateyinstall.ps1
@@ -3,19 +3,16 @@
 $packageName = $env:ChocolateyPackageName
 $packageTitle = $env:ChocolateyPackageTitle
 $packageVersion = $env:ChocolateyPackageVersion
-$installDir = Join-Path $(Get-ToolsLocation) $packageName
-$url64 = "https://sourceforge.net/projects/$packageName/files/$packageTitle-$packageVersion/$packageName-$packageVersion-win64-all-in-one.zip/download"
-$checksum64 = 'f8268f919dd87f138f4ce0f7b450da08a331b619'
-$checksumType64 = 'sha1'
 
-# Install package from all-in-one ZIP
-Install-ChocolateyZipPackage -PackageName $packageName -UnzipLocation $installDir `
-                             -Url64bit $url64 -checksum64 $checksum64 -checksumType64 $checksumType64
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  softwareName  = 'MComix'
+  url64bit      =  "https://sourceforge.net/projects/$packageName/files/$packageTitle-$packageVersion/$packageName-win64-$packageVersion.msi/download"
+  checksum64    = 'FIXME'
+  checksumType64=  'sha1'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
 
-# Create start menu link
-$extractedArchiveDir = Join-Path $installDir "$packageTitle-$packageVersion"
-$startMenuDir = Join-Path $([Environment]::GetFolderPath("StartMenu")) "Programs"
-Install-ChocolateyShortcut $(Join-Path $startMenuDir "$packageTitle.lnk") `
-                           $(Join-Path $extractedArchiveDir "$packageTitle.exe") 
-
-Write-Output "==> $packageTitle has been installed to '$installDir'."
+Install-ChocolateyPackage @packageArgs
